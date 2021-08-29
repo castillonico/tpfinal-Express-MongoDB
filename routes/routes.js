15e79d73cf1b79c838debc69bdcfe53858f03011
castillonico <nico.a.castillo@gmail.com>
2021-08-29 15:39:21 -0300

    Funciona bastate

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const router = express.Router(); 
-const bcrypt = require ("bcryptjs"); 
+const router = express.Router();
+const bcrypt = require("bcryptjs");
 const parser = require("body-parser");
 require("../src/connectDB");
 const User = require("../src/models/User");
+const jwt = require('jsonwebtoken'); 
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -17,10 +18,10 @@ router.use(parser.json());
 
 router.get('/', function (req, res, next) {
   console.log("Se accedio al path / ");
-  res.send('respond with a resource');  
+  res.send('respond with a resource');
 });
 
-router.get("/users", async (req, res) => { 
+router.get("/users", async (req, res) => {
   try {
     const arrayUsersDB = await User.find({});
     console.log(arrayUsersDB);
@@ -32,19 +33,42 @@ router.get("/users", async (req, res) => {
 
 router.post("/users", async (req, res) => {
   const body = req.body;
-  const pass = body.password; 
-  let passHashed = await bcrypt.hash (pass, 8); 
-  body.password = passHashed; 
-  console.log(body); 
+  const pass = body.password;
+  let passHashed = await bcrypt.hash(pass, 8);
+  body.password = passHashed;
+  console.log(body);
   try {
     const userDB = new User(body);
     await userDB.save();
-    console.log ("el nombre de usuario es: ", body.name); 
+    console.log("el nombre de usuario es: ", body.name);
     res.status(201).send("usuario Creado!");
   } catch (error) {
-    res.status(400).send("el usuario no se pudo crear"); 
-    console.log(error); 
+    res.status(400).send("el usuario no se pudo crear");
+    console.log(error);
   }
 });
+
+router.post("/user", async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    console.log("el usuario que intenta ingresar no existe"); 
+    return res.status(401).send("no existe el email");
+  } else {
+    console.log("el siguiente usuario quiere ingresar"); 
+    console.log("el usuario es: ", user);
+    bcrypt.compare(password, user.password, (err, sonIguales) => {
+      if (err) { 
+        console.log("por lo visto, salió un error ")
+        res.status(401).send(err)
+      } else {
+        console.log("parece que salió todo demasiado bien"); 
+        jwt.sing({user: user}, "secret", (err, token) => { 
+          res.status(200).send(token)
+      })}
+  })}
+});
+  
 
 module.exports = router;
