a8455a40fa10262443f18c583ad452c5a61ecbab
castillonico <nico.a.castillo@gmail.com>
2021-08-31 21:04:23 -0300

    login completo

--- routes/routes.js
+++ routes/routes.js
@@ -4,15 +4,10 @@ const bcrypt = require("bcryptjs");
 const parser = require("body-parser");
 require("../src/connectDB");
 const User = require("../src/models/User");
-const jwt = require('jsonwebtoken'); 
+const jwt = require('jsonwebtoken');
+const cors = require('cors'); 
 
-router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
+router.use(cors()); 
 router.use(parser.urlencoded({ extended: false }));
 router.use(parser.json());
 
@@ -21,15 +16,30 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/users", async (req, res) => {
+router.get("/users", verifyUser, async (req, res) => {
   try {
     const arrayUsersDB = await User.find({});
-    console.log(arrayUsersDB);
+    console.log("La cantidad de usuarios que enviarmos es: ", arrayUsersDB.length);
     res.send(arrayUsersDB)
   } catch (error) {
     console.log(error);
-  };
-});
+  }
+})
+
+function verifyUser(req, res, next) {
+  if (!req.headers['token']) {
+    res.status(400).send({message: 'No hay un token en el req '})
+  } else {
+    jwt.verify(req.headers['token'], "secret", (err) => {
+      if (err) {
+        console.log("error", err);
+        res.status(400).send({message: "token no válido "})
+      } else {
+        next()
+      }
+    })
+  }
+}
 
 router.post("/users", async (req, res) => {
   const body = req.body;
@@ -41,34 +51,44 @@ router.post("/users", async (req, res) => {
     const userDB = new User(body);
     await userDB.save();
     console.log("el nombre de usuario es: ", body.name);
-    res.status(201).send("usuario Creado!");
+    res.status(201).send({message: "usuario Creado!"});
   } catch (error) {
-    res.status(400).send("el usuario no se pudo crear");
+    res.status(400).send({message: "el usuario no se pudo crear"});
     console.log(error);
   }
 });
 
-router.post("/user", async (req, res) => {
+router.post("/login", async (req, res) => { 
   console.log(req.body);
-  const { email, password } = req.body;
+  const email = req.body.email;  
+  const password = req.body.password;
   const user = await User.findOne({ email });
   if (!user) {
-    console.log("el usuario que intenta ingresar no existe"); 
-    return res.status(401).send("no existe el email");
+    console.log("el usuario que intenta ingresar no existe");
+    return res.status(401).send({message: "el usuario que intenta ingresar no existe"});
   } else {
-    console.log("el siguiente usuario quiere ingresar"); 
     console.log("el usuario es: ", user);
     bcrypt.compare(password, user.password, (err, sonIguales) => {
-      if (err) { 
+      if (err) {
         console.log("por lo visto, salió un error ")
-        res.status(401).send(err)
-      } else {
-        console.log("parece que salió todo demasiado bien"); 
-        jwt.sing({user: user}, "secret", (err, token) => { 
-          res.status(200).send(token)
-      })}
-  })}
+        res.status(401).send({error: err})
+      } else
+        if (!sonIguales) {
+          console.log("El pass no es correcto");
+          res.status(401).send({message: "El pass es incorrecto"})
+        } else {
+          console.log("el usuario puede ingresar");
+          jwt.sign({ user: user }, "secret", (err, token) => {
+            if (err) {
+              console.log(err);
+              res.status(400).send({message: "se produjo un error al crear el token"})
+            } else {
+              res.status(200).send({token: token})
+            }
+          })
+        }
+    })
+  }
 });
-  
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,5 @@
 const express = require("express");
-const cors = require("cors"); 
 const app = express();
-
-
-app.use(cors()); 
 
 app.set("port", process.env.PORT || 3000);
 
